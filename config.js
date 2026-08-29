d97f24fa9e02d446f0d3364644e13126dffba69d
jisun <322230578+kohakukohaku15@users.noreply.github.com>
2026-08-29 14:32:14 +0900

    auto: save changes 2026-08-29 14:32:13

--- config.js
+++ config.js
@@ -3,3 +3,4 @@
 // service_role keyやSecret keyは絶対に設定しないでください。
 const SUPABASE_URL = "https://yqdawelkfrtmamovbowx.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_42y-WycgzZzyxo2Trr7FMw_8qYiBZaQ";
+const APP_PUBLIC_URL = "https://kohakukohaku15.github.io/cat-water-app/";
